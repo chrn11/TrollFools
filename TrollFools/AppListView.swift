76443ae433dd4dbdbc1c724e636bfe885a6dee3b
chrn11 <chrn11@users.noreply.github.com>
2026-04-17 01:29:54 +0800

    fix: remove duplicate code in body function

--- TrollFools/AppListView.swift
+++ TrollFools/AppListView.swift
@@ -110,25 +110,7 @@ var body: some View {
                     )
                 }
         }
-    }
-                    Button(role: .destructive) {
-                        selectorOpenedURL = result
-                        isWarningHidden = true
-                    } label: {
-                        Text(NSLocalizedString("Continue and Don’t Show Again", comment: ""))
-                    }
-                    Button(role: .cancel) {
-                        temporaryOpenedURL = nil
-                        isWarningPresented = false
-                    } label: {
-                        Text(NSLocalizedString("Cancel", comment: ""))
-                    }
-                } message: {
-                    Text(OptionView.warningMessage([$0.url]))
-                }
-        } else {
-            content
-        }
+}
     }
 
     var content: some View {
